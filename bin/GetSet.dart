d33b83a8172be0a31c77ca5d2d2e4dd008d05469
Your Name <hrk.sakib@gmail.com>
2021-03-18 00:51:48 +0600

    completed Dart

---
+++ bin/GetSet.dart
@@ -0,0 +1,7 @@
+import 'Objects/getset.dart';
+
+void main(List<String> args) {
+  var gtst = getset();
+  gtst.setName = "Sakib";
+  print(gtst.getName);
+}
---
+++ bin/Objects/getset.dart
@@ -0,0 +1,10 @@
+class getset {
+  String name = "";
+  void set setName(String s) {
+    name = s;
+  }
+
+  String get getName {
+    return name;
+  }
+}
